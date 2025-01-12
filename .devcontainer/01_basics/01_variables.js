da33227982f3d99f824feba27314bef6092b308f
Arya23D <bbinoyarya244@gmail.com>
2025-01-12 16:46:36 +0000

    variables

---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId= 1234
+let accountMail= "Arya@2gmail.com"
+var accountPassword= "Arya"
+accountCity = "Bengal"
+
+console.table([accountId,accountMail,accountPassword,accountCity])
+//comment ye use hota 
+/* ye poora para ya   iske andr sab ho jata comment 
+baar baar nahi likhna hota //
+*/
+
